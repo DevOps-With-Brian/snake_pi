867f368a7a7ab92fbe171033c07a90e6c36486ed
Brian Hopkins <brianhh1230@gmail.com>
2023-02-18 07:44:55 -0500

    feat(init): adding initial files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snake_pi
 Code running on a pi for snake room temps back up to faunadb
+
+# Fauna Access Key
+You will need to get an access key for the faunadb after you setup the db and then export that via:
+
+`export FAUNA_KEY=xxxxxx`
+
+Then after installing pre-req's mentioned above you can run:
+
+`python snake_temp.py` to have it check the temp and send it plus the time up to the graphQL endpoint of the faunadb.
---
+++ snake_temp.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+import requests
+import json
+from datetime import datetime
+import Adafruit_DHT as dht
+
+
+now = datetime.now() # current date and time
+load_dotenv()
+accessToken = os.getenv('FAUNA_KEY')
+endpoint = f"https://graphql.us.fauna.com/graphql"
+headers = {
+    'Authorization': f"Bearer {accessToken}"
+}
+DHT = 4
+h,t = dht.read_retry(dht.DHT22, DHT)
+
+
+current_time = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+temp = t *1.8 + 32
+
+query = """mutation {
+  createTemperature(
+    data: {
+          temp: %s
+          time: "%s"
+        }
+  ){
+    temp
+    time
+  }
+}""" % (int(temp), current_time)
+
+json_data = {
+    'query': query
+}
+
+print(json_data)
+
+r = requests.post(endpoint, headers=headers, json=json_data)
+if r.status_code == 200:
+    print(json.dumps(r.json(), indent=2))
+else:
+    raise Exception(f"Query failed to run with a {r.status_code}.")
